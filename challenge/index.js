3cdee8d01efbfdb9982cd27a44f1aba20fac939c
Betul Baskan <btlbsk@hotmail.com>
2022-01-12 22:27:46 -0700

    Initial commit

---
+++ challenge/index.js
@@ -0,0 +1,12 @@
+// Since the instruction asks to create an index.md file, I created js file with the same name.
+// Variable name(bookName) is assigned to the value( "War and Peace")- String data type.
+var bookName = "War and Peace"
+// Variable name (price) is assigned to the value (9- number data type).
+var price = 9
+// Variable name (shouldRead) is assigned to value (true-boolean data type)
+var shouldRead = true
+var numberPages = 256
+var color = "black"
+var shouldBuy = true
+// Variable name (aboutBook) is assigned to array .
+var aboutBook = ["history" , "action" , "love" , "peace"];
---
+++ challenge/index.md
@@ -0,0 +1,9 @@
+# Favorite Books
+
+War and Peace
+1984
+The little prince
+The Lord of the Rings
+Don Quixote
+
+Nobody is driver or navigator. But Olivia helped me by sharing her screen for cloning the repository. Once I tried to `git push` command, it didn't work well. 
